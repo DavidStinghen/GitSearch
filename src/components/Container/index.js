9472319a9e82ce8f55ead537d6f79fe82f09d923
DavidStinghen <davidstinghen@gmail.com>
2019-07-24 19:11:20 -0300

    fet: add container style

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -4,18 +4,19 @@ const Container = styled.div`
   max-width: 700px;
   background: #fff;
   border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0.1);
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
   padding: 30px;
   margin: 80px auto;
 
   h1 {
     font-size: 20px;
     display: flex;
+    flex-direction: row;
     align-items: center;
-  }
 
-  svg {
-    margin-right: 10px;
+    svg {
+      margin-right: 10px;
+    }
   }
 `;
 
